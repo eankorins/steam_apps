5875d5eb7bca3ea1a2c61aefbb0d658e390d7e49
eankorins <eankorins@gmail.com>
2014-09-16 21:20:34 +0200

    aaa

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module SteamApps
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.eager_load_paths += ["#{config.root}/lib"]
     config.eager_load_paths += ["#{config.root}/lib/**/"]
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
