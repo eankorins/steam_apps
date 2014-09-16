f5ac633fd19425c1c60bff78a719945f99a55f22
eankorins <eankorins@gmail.com>
2014-09-16 19:20:30 +0200

    AAA

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,8 +17,20 @@ after_fork do |server, worker|
   end
 
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq' }
+  end
 
+  unless Rails.env.production?
+    Sidekiq.configure_client do |config|
+      config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq'  }
+    end
+  end
   Sidekiq.configure_client do |config|
     config.redis = { size: 1, namespace: 'sidekiq' }
+
   end
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:11753612c96ed6a7442c8e4aa2e8c232@angelfish.redistogo.com:9159/'
+
+
 end
