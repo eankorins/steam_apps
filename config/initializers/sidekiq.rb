661786df4a3bdefb62765d89bd66e0f9210f1d0d
eankorins <eankorins@gmail.com>
2014-09-16 19:22:18 +0200

    AAA

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,3 @@
-ENV["REDISTOGO_URL"] = 'redis://redistogo:11753612c96ed6a7442c8e4aa2e8c232@angelfish.redistogo.com:9159/'
-
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDISTOGO_URL"], namespace: 'sidekiq' }
 end
